150bb9c0f7abab0dbc545a77aac952d99108675e
LeChacal <dev.lechacal@gmail.com>
2026-02-04 07:36:25 +0100

    Moved test to one file

---
+++ tests/all.test.ts
@@ -0,0 +1,32 @@
+import { after, before, test } from "node:test";
+
+import { Credentials, WebUntisClient } from "../src";
+
+let client: WebUntisClient;
+
+before(async () => {
+    const credentials = new Credentials(
+        "WebUntisClient",
+        "institut-saint-louis",
+        process.env.UNTIS_USERNAME!,
+        process.env.UNTIS_PASSWORD!,
+    );
+    client = new WebUntisClient(credentials);
+    await client.login();
+});
+
+after(async () => {
+    await client.logout();
+});
+
+test("get app data", async () => {
+    await client.getAppData();
+});
+
+test("get profile data", async () => {
+    await client.getProfile();
+});
+
+test("get own timetable", async () => {
+    await client.getTimetable(new Date("2026-02-05"), new Date("2026-02-06"));
+});
