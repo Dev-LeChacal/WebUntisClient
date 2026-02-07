c65336fcbcfeb24334846b5efeed274557d14322
LeChacal <dev.lechacal@gmail.com>
2026-02-07 16:10:05 +0100

    Revert "Removed exports in homeworks type"
    This reverts commit a7810a2243c8da43c61f8cfd1b629e92dc1cf967.

--- src/types/homeworks/homeworks.ts
+++ src/types/homeworks/homeworks.ts
@@ -1,10 +1,10 @@
-interface HomeworkRecord {
+export interface HomeworkRecord {
     homeworkId: number;
     teacherId: number;
     elementIds: number[];
 }
 
-interface Homeworks {
+export interface Homeworks {
     id: number;
     lessonId: number;
     date: number;
@@ -15,12 +15,12 @@ interface Homeworks {
     attachments: unknown[];
 }
 
-interface Teacher {
+export interface Teacher {
     id: number;
     name: string;
 }
 
-interface Lesson {
+export interface Lesson {
     id: number;
     subject: string;
     lessonType: string;
