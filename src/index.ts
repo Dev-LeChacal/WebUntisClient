59a9e49c906136f1a3a67f272012a71e8bdd5d31
LeChacal <dev.lechacal@gmail.com>
2026-04-07 07:37:59 +0200

    Export schools client

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { SchoolsClient } from "./clients/Schools";
 export { WebUntisClient } from "./clients/WebUntis";
 export * from "./structures";
 export type { SessionInfo } from "./types/session";
